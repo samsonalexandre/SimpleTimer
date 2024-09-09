806c61e46325c2a6b7937befa055a79c76c1e4b1
alexandresamson <alexander_samson@icloud.com>
2024-09-09 18:19:53 +0200

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Alexandre Samson on 09.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
