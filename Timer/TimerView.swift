a4a4f3257d633040d69fe9f33c2719b08c99ac3b
alexandresamson <alexander_samson@icloud.com>
2024-09-10 18:30:19 +0200

    Fertig

--- Timer/TimerView.swift
+++ Timer/TimerView.swift
@@ -8,8 +8,124 @@
 import SwiftUI
 
 struct TimerView: View {
+    
+    @StateObject private var timerVM = TimerViewModel()
+    
     var body: some View {
-        Text("Hello World")
+        if timerVM.isActive {
+            TimerCountdownView(timerVM: timerVM)
+                .frame(width: 350)
+        } else {
+            VStack {
+                TimerSelectionView(timerVM: timerVM)
+                    .frame(width: 300)
+                Button("Start") {
+                    timerVM.start()
+                }
+                .padding(.top)
+            }
+        }
+    }
+}
+
+struct TimerSelectionView: View {
+    
+    @ObservedObject var timerVM: TimerViewModel
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ZStack {
+                Text("Std.")
+                    .offset(x: 40)
+                Picker("Stunden", selection: $timerVM.stunden) {
+                    ForEach(0..<24) { stunde in
+                        Text(String(stunde))
+                            .tag(stunde)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .padding(.trailing, -16)
+                .clipped()
+            }
+            ZStack {
+                Text("Min.")
+                    .offset(x: 33)
+                Picker("Minuten", selection: $timerVM.minuten) {
+                    ForEach(0..<60) { minute in
+                        Text(String(minute))
+                            .tag(minute)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .padding(.horizontal, -16)
+                .clipped()
+            }
+            ZStack {
+                Text("Sek.")
+                    .offset(x: 25)
+                Picker("Sekunden", selection: $timerVM.sekunden) {
+                    ForEach(0..<60) { minute in
+                        Text(String(minute))
+                            .tag(minute)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .padding(.leading, -16)
+                .clipped()
+            }
+        }
+    }
+}
+
+struct TimerCountdownView: View {
+    
+    @ObservedObject var timerVM: TimerViewModel
+    
+    private let timer = Timer.publish(
+        every: 0.016,
+        on: .main,
+        in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            CircleProgressView(timerVM: timerVM)
+            VStack {
+                Text(timerVM.time)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Button("Reset") {
+                    timerVM.reset()
+                }
+                .padding(.top)
+            }
+        }
+        .onReceive(timer) { _ in
+            timerVM.update()
+        }
+    }
+}
+
+struct CircleProgressView: View {
+    
+    @ObservedObject var timerVM: TimerViewModel
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(
+                    .gray.opacity(0.2),
+                    style: StrokeStyle(lineWidth: 10)
+                )
+            Circle()
+                .trim(from: 0, to: timerVM.progress)
+                .stroke(
+                    .orange,
+                    style: StrokeStyle(
+                        lineWidth: 10,
+                        lineCap: .round)
+                )
+                .rotationEffect(.degrees(-90))
+        }
+            
     }
 }
 
--- Timer/TimerViewModel.swift
+++ Timer/TimerViewModel.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+class TimerViewModel: ObservableObject {
+    @Published var stunden = 0
+    @Published var minuten = 0
+    @Published var sekunden = 0
+    
+    private var stundenInitial = 0
+    private var minutenInitial = 0
+    private var sekundenInitial = 0
+    
+    @Published var time = "00:00:00"
+    @Published var progress = 0.0
+    @Published var isActive = false
+    
+    private var endDate = Date()
+    private var timerDuration = 0.0
+    
+    func start() {
+        isActive = true
+        
+        let calendar = Calendar.current
+        endDate = Date()
+
+        endDate = calendar.date(byAdding: .hour, value: stunden, to: endDate)!
+        endDate = calendar.date(byAdding: .minute, value: minuten, to: endDate)!
+        endDate = calendar.date(byAdding: .second, value: sekunden, to: endDate)!
+        
+        stundenInitial = stunden
+        minutenInitial = minuten
+        sekundenInitial = sekunden
+        
+        timerDuration = endDate.timeIntervalSince1970 - Date().timeIntervalSince1970
+        
+        update()
+    }
+    
+    func reset() {
+        isActive = false
+        stunden = stundenInitial
+        minuten = minutenInitial
+        sekunden = sekundenInitial
+        progress = 0
+    }
+    
+    func update() {
+        let diff = endDate.timeIntervalSince1970 - Date().timeIntervalSince1970
+        
+        if diff <= 0 {
+            isActive = false
+            return
+        }
+        
+        let date = Date(timeIntervalSince1970: diff)
+        
+        let calendar = Calendar.current
+        
+        let verbleibendeStunden = Int(diff/60/60) //calendar.component(.hour, from: date)
+        let verbleibendeMinuten = calendar.component(.minute, from: date)
+        let verbleibendeSekunden = calendar.component(.second, from: date)
+        print(verbleibendeStunden)
+        
+        progress = diff / timerDuration
+        time = String(format: "%d:%02d:%02d", verbleibendeStunden, verbleibendeMinuten, verbleibendeSekunden)
+    }
+}
+
