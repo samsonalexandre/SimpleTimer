497b2528cebee8645cda81476572d0752504f89b
alexandresamson <alexander_samson@icloud.com>
2024-09-10 18:29:10 +0200

    Timer

---
+++ Timer/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by Alexandre Samson on 09.09.24.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    TimerView()
+}
---
+++ Timer/TimerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimerViewModel.swift
+//  Timer
+//
+//  Created by Alexandre Samson on 09.09.24.
+//
+
+import Foundation
